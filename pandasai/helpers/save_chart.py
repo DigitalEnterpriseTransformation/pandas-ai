d0d88f58d727080718919bee48ec59af9323e728
Kang-Wangkai <wangkai@wintech-nano.com>
2024-07-16 09:22:04 +0800

    fix: indentation in image desensitization function call

--- pandasai/helpers/save_chart.py
+++ pandasai/helpers/save_chart.py
@@ -5,7 +5,7 @@ from .logger import Logger
 
 import matplotlib as mpl
 import matplotlib.font_manager as fm
-    
+
 def add_save_chart(
     code: str,
     logger: Logger,
@@ -32,24 +32,18 @@ def add_save_chart(
 
         save_charts_file = save_charts_path / f"{file_name}.png"
         
-        # to ensure the plt can display Chinese characters properly
-        if "\nplt" in code and "plt.rcParams" not in code:
-            # Find the index of the first occurrence of "\nplt"
-            import re
-            match = re.search(r"\n.*plt\.", code)
-            if match:
-                index = match.start()
-                code_to_add = "\nplt.rcParams['font.family'] = ['kkk', 'rrr']"
-                code = code[:index] + code_to_add + code[index:]
+        # # to ensure the plt can display Chinese characters properly
+        # if "\nplt" in code and "plt.rcParams" not in code:
+        #     # Find the index of the first occurrence of "\nplt"
+        #     import re
+        #     match = re.search(r"\n.*plt\.", code)
+        #     if match:
+        #         index = match.start()
+        #         code_to_add = "\nplt.rcParams['font.family'] = ['kkk', 'rrr']"
+        #         code = code[:index] + code_to_add + code[index:]
         
         # company name desensitization
-        if "plt.savefig" in code and "desensitized_plot" not in code:
-            index = code.find("plt.savefig")
-            if 'plt.subplots' in code and 'fig' in code:
-                code_to_add = "desensitized_plot(fig, use_subplots=True)\n" 
-            else:
-                code_to_add = "desensitized_plot(plt, )\n" 
-            code = code[:index] + code_to_add + code[index:]
+        code = insert_desensitized_plot_call(code)
             
         # to ensure the resulted plots are not cropped
         if "plt.savefig" in code and "bbox_inches='tight'" not in code:
@@ -65,4 +59,30 @@ def add_save_chart(
             code = code.replace("temp_chart.png", save_charts_file.as_posix())
             logger.log(f"Saving charts to {save_charts_file}")
 
+    return code
+
+def insert_desensitized_plot_call(code):
+    if "plt.savefig" in code and "desensitized_plot" not in code:
+        # Split the code into lines
+        lines = code.split('\n')
+        
+        for i, line in enumerate(lines):
+            if "plt.savefig" in line:
+                # Determine the indentation level of the plt.savefig line
+                indent = len(line) - len(line.lstrip())
+                indentation = ' ' * indent
+                
+                # Determine which desensitized_plot call to insert
+                if 'plt.subplots' in code and 'fig' in code:
+                    code_to_add = f"{indentation}desensitized_plot(fig, use_subplots=True)\n"
+                else:
+                    code_to_add = f"{indentation}desensitized_plot(plt)\n"
+                
+                # Insert the desensitized_plot call before the plt.savefig line
+                lines.insert(i, code_to_add)
+                break
+        
+        # Join the lines back into a single string
+        code = '\n'.join(lines)
+    
     return code
